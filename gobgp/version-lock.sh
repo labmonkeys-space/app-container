beb6c3af0d0bef4de99fa331eddf795afea6ce1b
Ronny Trommer <ronny@opennms.org>
2021-12-05 17:17:19 +0100

    Release GoBGP daemon 2.34.0

--- gobgp/version-lock.sh
+++ gobgp/version-lock.sh
@@ -4,4 +4,4 @@ set -u -o pipefail
 export VCS_SOURCE="$(git remote get-url --push origin)"
 export VCS_REVISION="$(git describe --always)"
 export BASE_IMAGE="quay.io/labmonkeys/alpine:3.15.0.b74"
-export GOBGP_VERSION="2.33.0"
+export GOBGP_VERSION="2.34.0"
