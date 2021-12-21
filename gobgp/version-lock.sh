d79b1808bf9867aa67e73dabb5a5c22cf85002ce
Ronny Trommer <ronny@opennms.org>
2021-12-21 16:28:20 +0100

    Bump GoBGP 3.0.0-rc2 to rc3

--- gobgp/version-lock.sh
+++ gobgp/version-lock.sh
@@ -4,4 +4,5 @@ set -u -o pipefail
 export VCS_SOURCE="$(git remote get-url --push origin)"
 export VCS_REVISION="$(git describe --always)"
 export BASE_IMAGE="quay.io/labmonkeys/alpine:3.15.0.b74"
-export GOBGP_VERSION="3.0.0-rc2"
+export GOBGP_VERSION="3.0.0-rc3"
+
