7eca947879b4edd7a6aee1afe0033b01e587141c
Ronny Trommer <ronny@opennms.org>
2022-06-27 18:31:18 +0200

    Update Horizon stream to latest develop

--- horizon-stream/version-lock.sh
+++ horizon-stream/version-lock.sh
@@ -7,6 +7,6 @@ DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
 export VCS_SOURCE
 export VCS_REVISION
 export DATE
-export BUILD_BASE_IMAGE="quay.io/labmonkeys/maven:jdk11.0.15-3.6.3.b440"
-export BASE_IMAGE="quay.io/labmonkeys/openjdk:jre-11.0.15.b139"
-export GIT_COMMIT="94f575ea"
+export BUILD_BASE_IMAGE="quay.io/labmonkeys/maven:jdk11.0.15-3.6.3.b459"
+export BASE_IMAGE="quay.io/labmonkeys/openjdk:jre-11.0.15.b141"
+export GIT_COMMIT="66c6a56"
