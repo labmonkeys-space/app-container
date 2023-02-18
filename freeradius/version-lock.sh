ca746d7f72c2fd97ef71e80559e1531d0b591d03
Ronny Trommer <ronny@opennms.org>
2023-02-18 22:03:35 +0100

    Update OpenSSL

--- freeradius/version-lock.sh
+++ freeradius/version-lock.sh
@@ -10,4 +10,4 @@ export DATE
 export BASE_IMAGE="quay.io/labmonkeys/alpine:3.17.2-20230218.b277"
 export FREERADIUS_VERSION="3.0.26-r0"
 export SQLITE_VERSION="3.40.1-r0"
-export OPENSSL_VERSION="3.0.7-r2"
+export OPENSSL_VERSION="3.0.8-r0"
