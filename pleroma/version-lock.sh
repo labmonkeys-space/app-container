b57b258670bcbf05027783ef0e2b94a14aeee2a3
Ronny Trommer <ronny@no42.org>
2026-09-23 11:54:25 +0000

    chore(deps): update alpine docker tag to v3.24.2
    Signed-off-by: Ronny Trommer <ronny@no42.org>

--- pleroma/version-lock.sh
+++ pleroma/version-lock.sh
@@ -10,7 +10,7 @@ export VCS_SOURCE
 export VCS_REVISION
 export DATE
 export BASE_IMAGE_BUILD="${LANG_ELIXIR}"
-export BASE_IMAGE="alpine:3.20.9"
+export BASE_IMAGE="alpine:3.24.2"
 export PLEROMA_VERSION="v2.10.2"
 
 # arm64 disabled: the Elixir/Erlang `mix release` hangs indefinitely under
