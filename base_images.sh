4163327d594cee0d1cf00d039766f1270effb71a
Ronny Trommer <ronny@no42.org>
2026-09-23 14:41:59 +0200

    chore(deps): update antora/antora docker tag to v3.2.0 (#103)
    Signed-off-by: Ronny Trommer <ronny@no42.org>

--- base_images.sh
+++ base_images.sh
@@ -4,7 +4,7 @@
 export OS_UBUNTU="ubuntu:noble-20260911"
 export OS_ALPINE="alpine:3.24"
 export OS_DEBIAN="debian:bookworm-20260918-slim"
-export APP_ANTORA="antora/antora:3.1.15"
+export APP_ANTORA="antora/antora:3.2.0"
 export LANG_JDK_11="quay.io/labmonkeys/openjdk:jdk-11.0.24.b186"
 export LANG_JRE_17="quay.io/labmonkeys/openjdk:jre-17.0.12.b187"
 export LANG_JDK_17="quay.io/labmonkeys/openjdk:jdk-17.0.12.b189"
